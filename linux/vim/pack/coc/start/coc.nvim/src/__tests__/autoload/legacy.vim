12d571a7b63908ca0bb0f7b1ec796731c2aec4b5
yoophoon <2420074212@qq.com>
2025-05-02 00:46:36 +0800

    将vim的第三方packageg由脚本安装，仓库不在包含这些第三方package文件

--- linux/vim/pack/coc/start/coc.nvim/src/__tests__/autoload/legacy.vim
+++
@@ -1,8 +0,0 @@
-
-function legacy#dict_add() dict
-  return self.key + 1
-endfunction
-
-function legacy#win_execute() abort
-  call win_execute(win_getid(), ['let w:foo = "a"."b"'])
-endfunction
--- linux/vim/pack/coc/start/coc.nvim/src/__tests__/modules/sandbox/log.js
+++
@@ -1,9 +0,0 @@
-const {wait, nvim} = require('coc.nvim')
-console.log('log')
-console.debug('debug')
-console.info('info')
-console.error('error')
-console.warn('warn')
-module.exports = () => {
-  return {wait, nvim}
-}
